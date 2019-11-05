6e765115c249ad96ba3ab8b10cf43c69c8252c62
James Vanstone <JV228882@falmouth.ac.uk>
2019-11-05 11:01:21 +0000

    Add AudioTinker.cs script for creating and playing tones. Create Audio_Scene.unity for playing the sounds in.

---
+++ Assets/YourProjectName/Scripts/AudioTinker.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using NaughtyAttributes;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class AudioTinker : MonoBehaviour {
+    private AudioSource audioSource;
+    private AudioClip outAudioClip;
+    public int freq = 1500;
+    
+    // Start is called before the first frame update
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+        outAudioClip = CreateToneAudioClip(freq);
+    }
+    
+
+    // Public APIs
+    public void PlayOutAudio() {
+        audioSource.PlayOneShot(CreateToneAudioClip(freq));
+    }
+
+
+    public void StopAudio() {
+        audioSource.Stop();
+    }
+    
+    
+    // Private 
+    private AudioClip CreateToneAudioClip(int frequency) {
+        int sampleDurationSecs = 5;
+        int sampleRate = 44100;
+        int sampleLength = sampleRate * sampleDurationSecs;
+        float maxValue = 1f / 4f;
+        
+        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
+        
+        float[] samples = new float[sampleLength];
+        for (var i = 0; i < sampleLength; i++) {
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float) i / (float) sampleRate));
+            float v = s * maxValue;
+            samples[i] = v;
+        }
+
+        audioClip.SetData(samples, 0);
+        return audioClip;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            PlayOutAudio();
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            freq += 100;
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            freq -= 100;
+        }
+    }
+
+    
+#if UNITY_EDITOR
+    //[Button("Save Wav file")]
+    private void SaveWavFile() {
+        string path = EditorUtility.SaveFilePanel("Where do you want the wav file to go?", "", "", "wav");
+        var audioClip = CreateToneAudioClip(1500);
+        SaveWavUtil.Save(path, audioClip);
+    }
+#endif
+}
