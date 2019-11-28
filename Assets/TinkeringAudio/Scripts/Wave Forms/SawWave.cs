796bbd7ce20297ede3d56cb748bc77fa0cf0dd4e
James Vanstone <jv228882@falmouth.ac.uk>
2019-11-28 16:09:39 +0000

    Saw Wave

---
+++ Assets/TinkeringAudio/Scripts/Wave Forms/SawWave.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawWave
+{
+    protected float freq;
+    protected float amp = 0.1f;
+    public AudioClip clip;
+
+    public SawWave(int frequency = 1)
+    {
+        freq = frequency;
+        MakeWave((int)freq, 0.5f);
+    }
+
+    // Generates the tone of the provided frequency with a square wave.
+    public void MakeWave(int frequency, float sampleDuration)
+    {
+        int sampleRate = 44200;
+        int sampleLength = (int)(sampleRate * sampleDuration);
+        float maxValue = amp;
+
+        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
+
+        float[] samples = new float[sampleLength];
+        for (var i = 0; i < sampleLength; i++)
+        {
+            float sample = Mathf.Tan(0.5f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float v = sample * maxValue;
+            samples[i] = v;
+        }
+        audioClip.SetData(samples, 0);
+        clip = audioClip;
+    }
+}
